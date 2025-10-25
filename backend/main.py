717fa9a4439fe5f1c92fb18f6e696b09cf76db91
shyamal-bhatt <shyamalbhatt20@gmail.com>
2025-10-24 20:45:37 -0400

    Backend FastAPI initialized

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+origins =[
+    "", # React Native Development Server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+    )
+
